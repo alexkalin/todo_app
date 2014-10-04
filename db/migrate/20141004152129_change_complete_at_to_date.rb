fc9ae79877df61af30568028cb5dab5ff013d71c
Alex Kalinchuk <alex.kalinchuk@gmail.com>
2014-10-04 11:23:27 -0400

    Change completed at to date

---
+++ db/migrate/20141004152129_change_complete_at_to_date.rb
@@ -0,0 +1,9 @@
+class ChangeCompleteAtToDate < ActiveRecord::Migration
+   def up
+    change_column :items, :completed_at, :date
+  end
+
+  def down
+    change_column :items, :completed_at, :datetime
+  end
+end
